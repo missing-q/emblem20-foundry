7199ba748881a78de64ded04e5037346a1726ad8
missing-q <ivybetterit@gmail.com>
2022-12-05 15:46:22 -0500

    and some other stuff too

--- module/emblem20.mjs
+++ module/emblem20.mjs
@@ -6,7 +6,7 @@ import { Emblem20ActorSheet } from "./sheets/actor-sheet.mjs";
 import { Emblem20ItemSheet } from "./sheets/item-sheet.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
-import { BOILERPLATE } from "./helpers/config.mjs";
+import { EMBLEM20 } from "./helpers/config.mjs";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -23,7 +23,7 @@ Hooks.once('init', async function() {
   };
 
   // Add custom constants for configuration.
-  CONFIG.BOILERPLATE = BOILERPLATE;
+  CONFIG.EMBLEM20 = EMBLEM20;
 
   /**
    * Set an initiative formula for the system
--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,103 @@
-export const BOILERPLATE = {};
+export const EMBLEM20 = {};
 
 /**
  * The set of Ability Scores used within the sytem.
  * @type {Object}
  */
- BOILERPLATE.abilities = {
-  "str": "BOILERPLATE.AbilityStr",
-  "dex": "BOILERPLATE.AbilityDex",
-  "con": "BOILERPLATE.AbilityCon",
-  "int": "BOILERPLATE.AbilityInt",
-  "wis": "BOILERPLATE.AbilityWis",
-  "cha": "BOILERPLATE.AbilityCha"
+ EMBLEM20.abilities = {
+  "str": "EMBLEM20.AbilityStr",
+  "dex": "EMBLEM20.AbilityDex",
+  "con": "EMBLEM20.AbilityCon",
+  "int": "EMBLEM20.AbilityInt",
+  "wis": "EMBLEM20.AbilityWis",
+  "cha": "EMBLEM20.AbilityCha"
 };
 
-BOILERPLATE.abilityAbbreviations = {
-  "str": "BOILERPLATE.AbilityStrAbbr",
-  "dex": "BOILERPLATE.AbilityDexAbbr",
-  "con": "BOILERPLATE.AbilityConAbbr",
-  "int": "BOILERPLATE.AbilityIntAbbr",
-  "wis": "BOILERPLATE.AbilityWisAbbr",
-  "cha": "BOILERPLATE.AbilityChaAbbr"
+EMBLEM20.abilityAbbreviations = {
+  "str": "EMBLEM20.AbilityStrAbbr",
+  "dex": "EMBLEM20.AbilityDexAbbr",
+  "con": "EMBLEM20.AbilityConAbbr",
+  "int": "EMBLEM20.AbilityIntAbbr",
+  "wis": "EMBLEM20.AbilityWisAbbr",
+  "cha": "EMBLEM20.AbilityChaAbbr"
 };
+
+EMBLEM20.weaponTypes = {
+  "sword": {
+    "label":"Sword",
+    "altlabel": "Katana",
+    "id":0,
+    "strength":[2],
+    "weak":[1],
+  },
+  "lance": {
+    "label":"Lance",
+    "altlabel": "Naginata",
+    "id":1,
+    "strength":[0],
+    "weak":[2]
+  },
+  "axe": {
+    "label":"Axe",
+    "altlabel": "Club",
+    "id":2,
+    "strength":[1],
+    "weak":[0]
+  },
+  "bow": {
+    "label":"Bow",
+    "altlabel": "Yumi",
+    "id":3,
+    "strength":[4],
+    "weak":[5]
+  },
+  "dagger": {
+    "label":"Dagger",
+    "altlabel": "Shuriken",
+    "id":4,
+    "strength":[5],
+    "weak":[3]
+  },
+  "gun": {
+    "label":"Gun",
+    "altlabel": "Tanegashima",
+    "id":5,
+    "strength":[3],
+    "weak":[4]
+  },
+  "anima": {
+    "label":"Anima Magic",
+    "altlabel": "Anima Magic",
+    "id":6,
+    "strength":[7],
+    "weak":[8]
+  },
+  "light": {
+    "label":"Light Magic",
+    "altlabel": "Light Magic",
+    "id":7,
+    "strength":[8],
+    "weak":[6]
+  },
+  "dark": {
+    "label":"Dark Magic",
+    "altlabel": "Dark Magic",
+    "id":8,
+    "strength":[6],
+    "weak":[7]
+  },
+  "stone": {
+    "label":"Stone",
+    "altlabel": "Claw",
+    "id":9,
+    "strength":[],
+    "weak":[]
+  },
+  "staff": {
+    "label":"Staff",
+    "altlabel": "Rod",
+    "id":10,
+    "strength":[],
+    "weak":[]
+  }
+}
